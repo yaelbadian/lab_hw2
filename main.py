b17505055a393b80d49f1f548b8883652052b658
YaelBadian <yael@proteantecs.com>
2020-05-29 17:20:16 +0300

    create preprocessing with trans

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import preprocessing
 import model
 import argparse
 from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
+
 
 
 if __name__ == '__main__':
@@ -10,7 +12,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     folder_path = args.input_folder
 
-    dataset = preprocessing.FaceMaskDataset(folder_path)
+    transformations = Compose([
+        ToPILImage(),
+        Resize((100, 100)),
+        ToTensor(),  # [0, 1]
+    ])
+
+    dataset = preprocessing.FaceMaskDataset(folder_path, transformations)
     data_loader = DataLoader(dataset, batch_size=8, shuffle=True)
     train_iter = iter(data_loader)
     images, labels = train_iter.next()
